b7ac96881b3eb64ad9d86cd138e6829def236336
AsifAliDeveloper <letmeetasif@email.com>
2025-12-09 18:31:51 +0500

    modify some file and delete

--- 03 _ pizza-menu/src/index.js
+++ 03 _ pizza-menu/src/index.js
@@ -72,10 +72,53 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numpizza = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+      {numpizza > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza
+                pizzaObj={pizza}
+                key={pizza.name}
+                // name={pizza.name}
+                // ingredients={pizza.ingredients}
+                // photoName={pizza.photoName}
+                // price={pizza.price}
+                // soldOut={pizza.soldOut}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>we're still working on our menu. Please come back later :)</p>
+      )}
+
+      {/* {numpizza > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza
+              pizzaObj={pizza}
+              key={pizza.name}
+              // name={pizza.name}
+              // ingredients={pizza.ingredients}
+              // photoName={pizza.photoName}
+              // price={pizza.price}
+              // soldOut={pizza.soldOut}
+            />
+          ))}
+        </ul>
+      )} */}
+
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -86,22 +129,23 @@ function Menu() {
         ingredients="Tomato, mushrooms"
         price={12}
         photoName="pizzas/funghi.jpg"
-      />
+      /> */}
     </main>
   );
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+  // if (pizzaObj.soldOut) return null;
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : " "}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
@@ -118,8 +162,33 @@ function Footer() {
   //   }
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're Currently Open.
+      {isOpen ? (
+        <Order openHour={openHour} closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+      {/* {isOpen && (
+        <div className="order">
+          <p>We're Open until {closeHour}:00. come visit us or order online.</p>
+          <button className="btn">order</button>
+        </div>
+      )} */}
+      {/* {new Date().toLocaleTimeString()}. We're Currently Open. */}
     </footer>
+  );
+}
+
+function Order({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're Open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">order</button>
+    </div>
   );
 }
 
