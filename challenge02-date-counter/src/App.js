7e3b98914a4402d5a7ed2ee512fd29947083f5d0
AsifAliDeveloper <letmeetasif@email.com>
2025-12-10 15:30:28 +0500

    add some file and update

--- challenge02-date-counter/src/App.js
+++ challenge02-date-counter/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <div className="app-container">
+      <h1 className="title">Date Counter App</h1>
       <Counter />
-    </>
+    </div>
   );
 }
 
@@ -14,27 +16,27 @@ function Counter() {
   const date = new Date("Decmber 10 2025");
   date.setDate(date.getDate() + count);
   return (
-    <>
-      <div>
+    <div className="counter-container">
+      <div className="step-controls">
         <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Step:{step}</span>
+        <span className="step-text">Step:{step}</span>
         <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
-      <div>
+      <div className="count-controls">
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count:{count}</span>
+        <span className="count-text">Count:{count}</span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
-      <p>
-        <span>
+      <p className="result-text">
+        <span className="result-text">
           {count === 0
             ? `Today is `
             : count > 0
             ? `${count} days from today is `
             : `${Math.abs(count)} days ago was `}
         </span>
-        <span>{date.toDateString()}</span>
+        <span className="date">{date.toDateString()}</span>
       </p>
-    </>
+    </div>
   );
 }
