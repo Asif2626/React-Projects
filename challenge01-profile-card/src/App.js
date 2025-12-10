84bd98fe5640749c2a14d50a5af5571c04d1de5d
AsifAliDeveloper <letmeetasif@email.com>
2025-12-10 16:35:25 +0500

    update file

--- challenge01-profile-card/src/App.js
+++ challenge01-profile-card/src/App.js
@@ -2,44 +2,75 @@ import React from "react";
 import "./App.css";
 import profileImage from "./profile.jpeg";
 
-function Card() {
+const skills = [
+  {
+    skills: "HTML + CSS",
+    level: "Advanced",
+    color: "#2662EA",
+  },
+  {
+    skills: "JavaScript",
+    level: "Intermediate",
+    color: "#EFD81D",
+  },
+  {
+    skills: "React",
+    level: "Beginner",
+    color: "#61c90cff",
+  },
+  {
+    skills: "JQuery",
+    level: "Intermediate",
+    color: "#E84F33",
+  },
+  {
+    skills: "Bootstrap",
+    level: "Intermediate",
+    color: "#60DAFB",
+  },
+  {
+    skills: "Git and GitHub",
+    level: "Beginner",
+    color: "#FF3B00",
+  },
+];
+
+function App() {
   return (
-    <div className="card">
-      <img
-        src={profileImage}
-        style={{ width: "250px", height: "250px" }}
-        alt="Profile"
-        className="img"
-      />
-
-      <h2 className="name">Asif Ali</h2>
-
-      <p className="bio">Front-End Web Developer and Learn React at Udemy.</p>
-
-      <div className="tags">
-        <span className="tag blue">HTML+CSS</span>
-        <span className="tag yellow">JavaScript</span>
-        <span className="tag orange">JQuery</span>
-        <span className="tag green">Web Design</span>
-        <span className="tag red">Git and GitHub</span>
-        <span className="tag cyan">React</span>
+    <div className="App">
+      <div className="centered-layout">
+        <Card />
       </div>
     </div>
   );
 }
 
-function App() {
+function Card() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#f0f0f0",
-      }}
-    >
-      <Card />
+    <div className="card">
+      <img src={profileImage} alt="Profile" className="img" />
+      <h2 className="name">Asif Ali</h2>
+      <p className="bio">
+        I have 1 year of experience in web development, including 6 months
+        focused on React.js cross-platform applications. I’ve built responsive
+        UIs, optimized performance, integrated APIs, and worked on projects
+        ranging from task management to logistics. I’m skilled in React concepts
+        like component architecture, state management, routing, and reusable
+        patterns, and I naturally excel at attention to detail, problem-solving,
+        and organizing tasks.
+      </p>
+
+      <div className="tags">
+        {skills.map((skill) => (
+          <span
+            key={skill.skills}
+            className="tag"
+            style={{ backgroundColor: skill.color }}
+          >
+            {skill.skills}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
