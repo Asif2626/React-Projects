db9ce88f226b787d382d7ceebf095f89885d8d76
AsifAliDeveloper <letmeetasif@email.com>
2026-01-25 02:16:44 +0500

    Work on dashboard

--- 17-The-Wild-Oasis/src/ui/AppLayout.jsx
+++ 17-The-Wild-Oasis/src/ui/AppLayout.jsx
@@ -7,13 +7,14 @@ const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
-  height: 128vh;
+  height: 100vh;
 `;
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
-  overflow: scroll;
+  height: 100%;
+  overflow-y: auto;
 `;
 
 const Container = styled.div`
