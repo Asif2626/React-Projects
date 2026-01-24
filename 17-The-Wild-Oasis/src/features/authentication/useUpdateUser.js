bd0e928b7eb4afa73422faa980e78408cf7f9d61
AsifAliDeveloper <letmeetasif@email.com>
2026-01-24 23:34:02 +0500

    Light-Mode and Dark-Mode

--- 17-The-Wild-Oasis/src/features/authentication/useUpdateUser.js
+++ 17-The-Wild-Oasis/src/features/authentication/useUpdateUser.js
@@ -11,7 +11,6 @@ export function useUpdateUser() {
     onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
       queryClient.setQueriesData(["user"], user);
-      //   queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (err) =>
       toast.error(
